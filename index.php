8deb1a46adb325e95dd7b469b0106b1912b126e4
sigit <sigit.pamungkas@whnmandiri.co.id>
2016-07-30 20:29:45 +0700

    Update Tutorial enkripsi

--- index.php
+++ index.php
@@ -1,48 +1,15 @@
 <?php
 	//untuk enkripsi
 	include "./lib/function.php";
-?>
-<html>
-<head>
-	<title>Enkripsi URL</title>
-</head>
-<body>
-<form action="proses.php?<?php echo paramEncrypt('nama=agus&blog=ri32')?>" method="post">
-<table>
-	<tr>
-		<td>Email</td><td><input type="email"  name="email" size="33"/></td>
-	</tr>
-	<tr>
-		<td>Pesan</td><td><textarea cols="25" rows="3" name="pesan"></textarea>
-	</tr>
-	<tr>
-		<td><input type="submit" name="test" value="test" /></td><td></td>
-	</tr>
-</table>
-<p><strong>Menu Link Terenkripsi :</strong></p>
-<ul>
-  <li><a href="?<?php echo paramEncrypt('page=menu_satu')?>" title="">Menu SATU</a></li>
-  <li><a href="?<?php echo paramEncrypt('page=menu_dua')?>" title="">Menu DUA</a>.</li>
-</ul>
-</form>
-</body>
-</html>
-<br>
 
-<?php
-	//untuk mendecode url yang di enrypsi
-	$var=decode($_SERVER['REQUEST_URI']);
 
-	//pecahkan nilai array
-	$page=$var['page'];
+	 $abc 			 = 'abcdefgh';
+	 $samadengan = "=";
+	 $dengan		 = "&";
+	 $numeric		 = "12345";
 
-	//concate dengan nama file
-	$halaman="$page.php";
-
-	//jika file yang diinclude tidak ada.
-	if(!file_exists($halaman) || empty($page)){
-		//include "welcome.php";
-	}else{
-		include "$halaman";
-	}
+	echo "ini adalah enkripsi dari <strong>$abc</strong> menjadi <strong>".paramEncrypt($abc)."</strong><br/>" ;
+	echo "ini adalah enkripsi dari <strong>$samadengan</strong> menjadi <strong>".paramEncrypt($samadengan)."</strong><br/>" ;
+	echo "ini adalah enkripsi dari <strong>$dengan</strong> menjadi <strong>".paramEncrypt($dengan)."</strong><br/>" ;
+	echo "ini adalah enkripsi dari <strong>$numeric</strong> menjadi <strong>".paramEncrypt($numeric)."</strong><br/>" ;
 ?>
--- readme.txt
+++ readme.txt
@@ -1,1 +1,27 @@
-metode enkripsi dengan AES
+################################################################################
+################################################################################
+####                    ENKRIPSI METODE AES                                 ####
+####                    MASTERFREELANCE.COM                                 ####
+####                                                                        ####
+####                                                                        ####
+####                                                                        ####
+####                     copyright 2016                                     ####
+################################################################################
+################################################################################
+
+
+<?php
+	//untuk enkripsi
+	include "./lib/function.php";
+
+
+	 $abc 			 = 'abcdefgh';
+	 $samadengan = "=";
+	 $dengan		 = "&";
+	 $numeric		 = "12345";
+
+	echo "ini adalah enkripsi dari <strong>$abc</strong> menjadi <strong>".paramEncrypt($abc)."</strong><br/>" ;
+	echo "ini adalah enkripsi dari <strong>$samadengan</strong> menjadi <strong>".paramEncrypt($samadengan)."</strong><br/>" ;
+	echo "ini adalah enkripsi dari <strong>$dengan</strong> menjadi <strong>".paramEncrypt($dengan)."</strong><br/>" ;
+	echo "ini adalah enkripsi dari <strong>$numeric</strong> menjadi <strong>".paramEncrypt($numeric)."</strong><br/>" ;
+?>
